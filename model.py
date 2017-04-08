d6c0cb5416bee40a2fef3f6e59b28d97b5cd85cf
prateek medi <medisp@icloud.com>
2017-04-08 14:47:41 -0400

    first commit

---
+++ model.py
@@ -0,0 +1,127 @@
+# Load pickled data
+import pickle
+import numpy as np
+import tensorflow as tf
+tf.python.control_flow_ops = tf
+import csv
+import cv2
+
+
+'''
+with open('small_train_traffic.p', mode='rb') as f:
+    data = pickle.load(f)
+
+X_train, y_train = data['features'], data['labels']
+'''
+# Loading Data:
+lines = []
+with open('./data/driving_log.csv') as drivinglog:
+	reader = csv.reader(drivinglog)
+	for line in reader:
+		lines.append(line)
+			
+
+images = []
+measurements = []
+for line in lines[1:]:
+	source_path=line[0]
+	filename = source_path.split('/')[-1]
+	current_path = './data/IMG/' + filename
+	image = cv2.imread(current_path)
+	images.append(image)
+	
+	measurement = float(line[3])
+	measurements.append(measurement)
+
+augmented_images, augmented_measurements = [] , []
+for image, measurement in zip(images.measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement*-1.0)
+
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+	
+
+# Initial Setup for Keras
+	
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+#from keras.layers.core import Dense, Activation, Flatten, Dropout
+#from keras.layers.convolutional import Convolution2D
+#from keras.layers.pooling import MaxPooling2D
+
+# TODO: Build the Final Test Neural Network in Keras Here
+
+model = Sequential()
+# Normalization and mean centering using lambda layer
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape = (160,320,3)))
+model.add(Cropping2D(cropping=((75,25),(0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64, 5, 5, subsample=(2,2), activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(1))
+
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+
+# Saving model
+model.save('model.h5')
+
+'''
+model.add(Convolution2D(32, 3, 3, input_shape=(32, 32, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(5))
+model.add(Activation('softmax'))
+
+# preprocess data
+
+X_normalized = np.array(X_train / 255.0 - 0.5 )
+
+from sklearn.preprocessing import LabelBinarizer
+label_binarizer = LabelBinarizer()
+y_one_hot = label_binarizer.fit_transform(y_train)
+
+model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+history = model.fit(X_normalized, y_one_hot, nb_epoch=10, validation_split=0.2)
+
+with open('small_test_traffic.p', 'rb') as f:
+    data_test = pickle.load(f)
+
+X_test = data_test['features']
+y_test = data_test['labels']
+
+# preprocess data
+X_normalized_test = np.array(X_test / 255.0 - 0.5 )
+y_one_hot_test = label_binarizer.fit_transform(y_test)
+
+print("Testing")
+
+# TODO: Evaluate the test data in Keras Here
+metrics = model.evaluate(X_normalized_test,y_one_hot_test)
+# TODO: UNCOMMENT CODE
+for metric_i in range(len(model.metrics_names)):
+    metric_name = model.metrics_names[metric_i]
+    metric_value = metrics[metric_i]
+    print('{}: {}'.format(metric_name, metric_value))  
+	
+#addtl
+'''
